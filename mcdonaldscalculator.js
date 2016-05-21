410ead547a5eefdcf0e99ebd85dc018c3a6eeafe
NotAnOctopus <zapmeister999@hotmail.co.uk>
2016-05-21 22:27:55 +0100

    Create mcdonaldscalculator.js

---
+++ mcdonaldscalculator.js
@@ -0,0 +1,60 @@
+var bitcoins_are_amazing = ['Aberdeen,Scotland', 57.15, -2.15, 0.0, 'Adelaide,Australia', -34.9166666667, 138.6, 9.5, 'Algiers,Algeria', 36.8333333333, 3.0, 1.0, 'Amsterdam,Netherlands', 52.3666666667, 4.88333333333, 1.0, 'Ankara,Turkey', 39.9166666667, 32.9166666667, 2.0, 'Asuncion,Paraguay', -25.25, -57.6666666667, -4.0, 'Athens,Greece', 37.9666666667, 23.7166666667, 2.0, 'Auckland,NewZealand', -36.8666666667, 174.75, 12.0, 'Bangkok,Thailand', 13.75, 100.5, 7.0, 'Barcelona,Spain', 41.3833333333, 2.15, 1.0, 'Beijing,China', 39.9166666667, 116.416666667, 8.0, 'Belem,Brazil', -1.46666666667, -48.4833333333, -3.0, 'Belfast,NorthernIreland', 54.6166666667, -5.93333333333, 0.0, 'Belgrade,Serbia', 44.8666666667, 20.5333333333, 1.0, 'Berlin,Germany', 52.5, 13.4166666667, 1.0, 'Birmingham,England', 52.4166666667, -1.91666666667, 0.0, 'Bogota,Colombia', 4.53333333333, -74.25, -5.0, 'Bombay,India', 19.0, 72.8, 5.5, 'Bordeaux,France', 44.8333333333, -0.516666666667, 1.0, 'Bremen,Germany', 53.0833333333, 8.81666666667, 1.0, 'Brisbane,Australia', -27.4833333333, 153.133333333, 10.0, 'Bristol,England', 51.4666666667, -2.58333333333, 0.0, 'Brussels,Belgium', 50.8666666667, 4.36666666667, 1.0, 'Bucharest,Romania', 44.4166666667, 26.1166666667, 2.0, 'Budapest,Hungary', 47.5, 19.0833333333, 1.0, 'BuenosAires,Argentina', -34.5833333333, -58.3666666667, -3.0, 'Cairo,Egypt', 30.0333333333, 31.35, 2.0, 'Calcutta,India', 22.5666666667, 88.4, 5.5, 'Canton,China', 23.1166666667, 113.25, 8.0, 'CapeTown,SouthAfrica', -33.9166666667, 18.3666666667, 2.0, 'Caracas,Venezuela', 10.4666666667, -67.0333333333, -4.0, 'Cayenne,FrenchGuiana', 4.81666666667, -52.3, -3.0, 'Chihuahua,Mexico', 28.6166666667, -106.083333333, -7.0, 'Chongqing,China', 29.7666666667, 106.566666667, 8.0, 'Copenhagen,Denmark', 55.6666666667, 12.5666666667, 1.0, 'Cordoba,Argentina', -31.4666666667, -64.1666666667, -3.0, 'Dakar,Senegal', 14.6666666667, -17.4666666667, 0.0, 'Darwin,Australia', -12.4666666667, 130.85, 9.5, 'Djibouti,Djibouti', 11.5, 43.05, 3.0, 'Dublin,Ireland', 53.3333333333, -6.25, 0.0, 'Durban,SouthAfrica', -29.8833333333, 30.8833333333, 2.0, 'Edinburgh,Scotland', 55.9166666667, -3.16666666667, 0.0, 'Frankfurt,Germany', 50.1166666667, 8.68333333333, 1.0, 'Georgetown,Guyana', 6.75, -58.25, -4.0, 'Glasgow,Scotland', 55.8333333333, -4.25, 0.0, 'GuatemalaCity,Guatemala', 14.6166666667, -90.5166666667, -6.0, 'Guayaquil,Ecuador', -2.16666666667, -79.9333333333, -5.0, 'Hamburg,Germany', 53.55, 10.0333333333, 1.0, 'Hammerfest,Norway', 70.6333333333, 23.6333333333, 1.0, 'Havana,Cuba', 23.1333333333, -82.3833333333, -5.0, 'Helsinki,Finland', 60.1666666667, 25.0, 2.0, 'Hobart,Tasmania', -42.8666666667, 147.316666667, 10.0, 'HongKong,China', 22.3333333333, 114.183333333, 8.0, 'Iquique,Chile', -20.1666666667, -70.1166666667, -4.0, 'Irkutsk,Russia', 52.5, 104.333333333, 8.0, 'Jakarta,Indonesia', -6.26666666667, 106.8, 7.0, 'Johannesburg,SouthAfrica', -26.2, 28.0666666667, 2.0, 'Kingston,Jamaica', 17.9833333333, -76.8166666667, -5.0, 'Kinshasa,Congo', -4.3, 15.2833333333, 1.0, 'KualaLumpur,Malaysia', 3.13333333333, 101.7, 8.0, 'LaPaz,Bolivia', -16.45, -68.3666666667, -4.0, 'Leeds,England', 53.75, -1.5, 0.0, 'Lima,Peru', -12.0, -77.0333333333, -5.0, 'Lisbon,Portugal', 38.7333333333, -9.15, 0.0, 'Liverpool,England', 53.4166666667, -3.0, 0.0, 'London,England', 51.5333333333, -0.0833333333333, 0.0, 'Lyons,France', 45.75, 4.83333333333, 1.0, 'Madrid,Spain', 40.4333333333, -3.7, 1.0, 'Manchester,England', 53.5, -2.25, 0.0, 'Manila,Philippines', 14.5833333333, 120.95, 8.0, 'Marseilles,France', 43.3333333333, 5.33333333333, 1.0, 'Mazatlan,Mexico', 23.2, -106.416666667, -7.0, 'Mecca,SaudiArabia', 21.4833333333, 39.75, 3.0, 'Melbourne,Australia', -37.7833333333, 144.966666667, 10.0, 'MexicoCity,Mexico', 19.4333333333, -99.1166666667, -6.0, 'Milan,Italy', 45.45, 9.16666666667, 1.0, 'Montevideo,Uruguay', -34.8833333333, -56.1666666667, -3.0, 'Moscow,Russia', 55.75, 37.6, 3.0, 'Munich,Germany', 48.1333333333, 11.5833333333, 1.0, 'Nagasaki,Japan', 32.8, 129.95, 9.0, 'Nagoya,Japan', 35.1166666667, 136.933333333, 9.0, 'Nairobi,Kenya', -1.41666666667, 36.9166666667, 3.0, 'Nanjing,China', 32.05, 118.883333333, 8.0, 'Naples,Italy', 40.8333333333, 14.25, 1.0, 'NewDelhi,India', 28.5833333333, 77.2, 5.5, 'Newcastle-on-Tyne,England', 54.9666666667, -1.61666666667, 0.0, 'Odessa,Ukraine', 46.45, 30.8, 2.0, 'Osaka,Japan', 34.5333333333, 135.5, 9.0, 'Oslo,Norway', 59.95, 10.7, 1.0, 'PanamaCity,Panama', 8.96666666667, -79.5333333333, -5.0, 'Paramaribo,Suriname', 5.75, -55.25, -3.0, 'Paris,France', 48.8, 2.33333333333, 1.0, 'Perth,Australia', -31.95, 115.866666667, 8.0, 'Plymouth,England', 50.4166666667, -4.08333333333, 0.0, 'PortMoresby,PapuaNewGuinea', -9.41666666667, 147.133333333, 10.0, 'Prague,CzechRepublic', 50.0833333333, 14.4333333333, 1.0, 'Rangoon,Myanmar', 16.8333333333, 96.0, 6.5, 'Reykjavik,Iceland', 64.0666666667, -21.9666666667, 0.0, 'RiodeJaneiro,Brazil', -22.95, -43.2, -3.0, 'Rome,Italy', 41.9, 12.45, 1.0, 'Salvador,Brazil', -12.9333333333, -38.45, -3.0, 'Santiago,Chile', -33.4666666667, -70.75, -4.0, 'St.Petersburg,Russia', 59.9333333333, 30.3, 3.0, 'SaoPaulo,Brazil', -23.5166666667, -46.5166666667, -3.0, 'Shanghai,China', 31.1666666667, 121.466666667, 8.0, 'Singapore,Singapore', 1.23333333333, 103.916666667, 8.0, 'Sofia,Bulgaria', 42.6666666667, 23.3333333333, 2.0, 'Stockholm,Sweden', 59.2833333333, 18.05, 1.0, 'Sydney,Australia', -34.0, 151.0, 10.0, 'Tananarive,Madagascar', -18.8333333333, 47.55, 3.0, 'Teheran,Iran', 35.75, 51.75, 3.5, 'Tokyo,Japan', 35.6666666667, 139.75, 9.0, 'Tripoli,Libya', 32.95, 13.2, 2.0, 'Venice,Italy', 45.4333333333, 12.3333333333, 1.0, 'Veracruz,Mexico', 19.1666666667, -96.1666666667, -6.0, 'Vienna,Austria', 48.2333333333, 16.3333333333, 1.0, 'Vladivostok,Russia', 43.1666666667, 132.0, 10.0, 'Warsaw,Poland', 52.2333333333, 21.0, 1.0, 'Wellington,NewZealand', -41.2833333333, 174.783333333, 12.0, 'Zurich,Switzerland', 47.35, 8.51666666667, 1.0];
+
+function thingy(place, lat, longi, time) {
+  this.place=place;
+  this.lat=lat;
+  this.longi=longi;
+  this.time=time;
+}
+
+var thingamabob=[];
+for (tomato=0; tomato<bitcoins_are_amazing.length/4; tomato++) {
+  thingamabob.push(new thingy(bitcoins_are_amazing[tomato*4], bitcoins_are_amazing[tomato*4+1], bitcoins_are_amazing[tomato*4+2], bitcoins_are_amazing[tomato*4+3]))
+}
+
+var cake = Math.PI; // cake tastes better than pie
+
+var mcdonaldsopen = 6;
+var mcdonaldsclose = 10.5;
+
+var timediff;
+var arrivetime;
+var lat1;
+var long1;
+var timezonehere;
+
+var snowmen_are_creepy = ['avril lavigne is weird', 77777777777] // she really is
+var chicken_monkey_duck;
+
+function distance(lat1, long1, lat2, long2) {
+  return 6371.01 * (Math.acos((Math.cos(lat1/180*cake) * Math.cos(lat2/180*cake) * Math.cos(long1/180*cake - long2/180*cake)) + (Math.sin(lat1/180*cake) * Math.sin(lat2/180*cake))))
+}
+
+function answer(lat1, long1, lat2, long2, currenttime, timezonehere, timezonethere, flightspeed) {
+  timediff=distance(lat1, long1, lat2, long2)/flightspeed;
+  arrivetime=(24+currenttime+timediff+timezonethere-timezonehere)%24; // WHY THE FUCK is (-10)%24 = -10 in js, and not 14?
+  if (arrivetime>mcdonaldsopen && arrivetime<mcdonaldsclose) { return timediff }
+  else { return (24+mcdonaldsopen-arrivetime)%24+timediff }
+}
+
+function why_not_a_little_spanish_flea() {
+  var currentlocation = document.getElementById("there was a little spanish flea").value;
+  var currenttime = +document.getElementById("a record star he thought he'd be").value + +document.getElementById("he'd heard of singers like beatles").value/60;
+  var flightspeed = +document.getElementById("and chipmunks he'd seen on tv").value;
+  for (x in thingamabob) {
+    if (thingamabob[x].place == currentlocation) {
+      lat1=thingamabob[x].lat;
+      long1=thingamabob[x].longi;
+      timezonehere=thingamabob[x].time
+    }
+  }
+  for (z in thingamabob) {
+    chicken_monkey_duck = answer(lat1, long1, thingamabob[z].lat, thingamabob[z].longi, currenttime, timezonehere, thingamabob[z].time, flightspeed)
+    if (chicken_monkey_duck < snowmen_are_creepy[1]) {
+      snowmen_are_creepy=[thingamabob[z].place, chicken_monkey_duck]
+    }
+  }
+  snowmen_are_creepy[1] += 0.00000001 // fucking floating point errors
+  document.getElementById("batman").innerHTML = 'Your fastest route to get a hold of a McDonald\'s Breakfast would be to go to ' + snowmen_are_creepy[0] + ' and it would take you ' + Math.floor(snowmen_are_creepy[1]) + ' hours and ' + Math.floor((snowmen_are_creepy[1]-Math.floor(snowmen_are_creepy[1]))*60) + ' minutes'
+  snowmen_are_creepy = ['avril lavigne is weird', 77777777777]
+}
